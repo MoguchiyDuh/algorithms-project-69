76c21b5a54b50d753e060483f5e28d7421f8abc5
Moguchiy_Duh <kirnekrasov12@gmail.com>
2024-10-20 06:22:27 +0200

    bug fix & multiple words parsing awailable

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -1,11 +1,31 @@
 import re
+import string
 
 
-def search(docs: dict, str_to_find: str) -> list:
-    result = []
-    query = re.escape(str_to_find.rstrip(".,!?"))
+def search(docs: dict, words: str) -> list:
+    result = {}
+
+    def sort_by_values_desc(dict_to_sort: dict) -> list:
+        return [
+            key
+            for key, value in sorted(
+                dict_to_sort.items(), key=lambda item: item[1], reverse=True
+            )
+        ]
+
+    translator = str.maketrans("", "", string.punctuation)
+    words_list = words.translate(translator).split(" ")
     for doc in docs:
-        pattern = rf"\b{query}\b"
-        if re.findall(pattern, doc["text"], re.IGNORECASE):
-            result.append(doc["id"])
-    return result
+        print(doc)
+        number_of_entries = 0
+        for word in words_list:
+            print(word)
+            pattern = rf"\b{word}\b"
+            entries = re.findall(pattern, doc["text"], re.IGNORECASE)
+            if entries:
+                number_of_entries += len(entries)
+        if number_of_entries != 0:
+            result[doc["id"]] = number_of_entries
+            print(number_of_entries)
+
+    return sort_by_values_desc(result)
--- tests/test_search_engine.py
+++ tests/test_search_engine.py
@@ -1,25 +1,27 @@
 from search_engine.search_engine import search
 
+doc1 = "I can't shoot straight unless I've had a pint!"
+doc2 = "Don't shoot shoot shoot that thing at me."
+doc3 = "I'm your shooter."
+docs = [
+    {"id": "doc1", "text": doc1},
+    {"id": "doc2", "text": doc2},
+    {"id": "doc3", "text": doc3},
+]
+
 
 def test_1():
-    doc1 = "I can't shoot straight unless I've had a pint!"
-    doc2 = "Don't shoot shoot shoot that thing at me."
-    doc3 = "I'm your shooter."
-
-    docs = [
-        {"id": "doc1", "text": doc1},
-        {"id": "doc2", "text": doc2},
-        {"id": "doc3", "text": doc3},
-    ]
     word = "shoot"
-
     result = search(docs, word)
-    assert result == ["doc1", "doc2"]
+    assert result == ["doc2", "doc1"]
 
 
 def test_2():
-    doc1 = {"id": "doc1", "text": "I can't shoot straight unless I've had a pint!"}
-    docs = [doc1]
+    docs2 = [{"id": "doc1", "text": doc1}]
+    assert search(docs2, "pint") == ["doc1"]
+    assert search(docs2, "pint!") == ["doc1"]
 
-    assert search(docs, "pint") == ["doc1"]
-    assert search(docs, "pint!") == ["doc1"]
+
+def test_3():
+    result = search(docs, "shoot at me")
+    assert result == ["doc2", "doc1"]
